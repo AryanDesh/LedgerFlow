f4d244d54d8d0d7706dc8ab4166a5c400c96b3cb
Aryan Deshmukh <aryan.deshmukh21@vit.edu>
2024-09-22 23:43:53 +0530

    Added dashboard template

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,11 +1,12 @@
-'use client'
-import { signIn, signOut, useSession} from "next-auth/react"
-import {Appbar} from "@repo/ui/Appbar"
-export default function Home() {
-  const session = useSession();
-  return (
-    <div className='text-3xl'>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}></Appbar>
-    </div>
-  );
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
 }
